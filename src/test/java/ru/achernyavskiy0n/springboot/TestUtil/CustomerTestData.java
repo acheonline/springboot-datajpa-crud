e20fb3c5c5022ee80629e1671753742c4fe5cce6
AntonChernyavskiy <chernyavskiy.in@gmail.com>
2020-08-04 09:55:31 +0300

    add CustomerTestData and fix CustomerServiceTest

---
+++ src/test/java/ru/achernyavskiy0n/springboot/TestUtil/CustomerTestData.java
@@ -0,0 +1,27 @@
+package ru.achernyavskiy0n.springboot.TestUtil;
+
+import ru.achernyavskiy0n.springboot.model.customer.Customer;
+import static ru.achernyavskiy0n.springboot.model.customer.Customer.START_SEQ;
+
+/**
+ * 04.08.2020
+ *
+ * @author a.chernyavskiy0n
+ */
+public class CustomerTestData {
+    public static TestMatcher<Customer> CUSTOMER_MATCHER = TestMatcher.usingEquals(Customer.class);
+
+    public static final int CUSTOMER_ID1 = START_SEQ;
+    public static final int CUSTOMER_ID2 = START_SEQ + 1;
+    public static final int CUSTOMER_ID3 = START_SEQ + 2;
+
+    public static final Customer CUSTOMER_1 = new Customer(CUSTOMER_ID1, "User");
+    public static final Customer CUSTOMER_2 = new Customer(CUSTOMER_ID2, "Admin");
+    public static final Customer CUSTOMER_3 = new Customer(CUSTOMER_ID3, "God");
+
+    public static final Customer NEW_CUSTOMER = new Customer(100003, "new_customer");
+    public static final int NEW_CUSTOMER_ID = START_SEQ + 3;
+
+    public static final Customer UPDATED_CUSTOMER = new Customer(CUSTOMER_ID1, "updated");
+
+}
--- src/test/java/ru/achernyavskiy0n/springboot/service/customer/CustomerServiceTest.java
+++ src/test/java/ru/achernyavskiy0n/springboot/service/customer/CustomerServiceTest.java
@@ -1,26 +1,19 @@
 package ru.achernyavskiy0n.springboot.service.customer;
 
-import org.hibernate.Hibernate;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-import ru.achernyavskiy0n.springboot.SpringBootStarterExample;
 import ru.achernyavskiy0n.springboot.model.customer.Customer;
-import ru.achernyavskiy0n.springboot.service.customer.CustomerService;
 
-import java.util.Arrays;
 import java.util.List;
+
+import static ru.achernyavskiy0n.springboot.TestUtil.CustomerTestData.*;
 
 /**
  * 31.07.2020
@@ -38,43 +31,35 @@ public class CustomerServiceTest {
 
     @Test
     public void findById() {
-        Customer customer = service.findById(100002);
-        Customer expected = new Customer(100002, "God");
-        Assert.assertEquals(expected, customer);
+        Customer customer = service.findById(CUSTOMER_ID3);
+        Assert.assertEquals(CUSTOMER_3, customer);
     }
 
     @Test
     public void save() throws Exception {
-        Customer newCustomer = new Customer(100003, "Anton");
+        Customer newCustomer = NEW_CUSTOMER;
         Customer created = service.save(newCustomer);
         int newId = created.getId();
         newCustomer.setId(newId);
-        Assert.assertEquals(newCustomer, service.findById(100003));
+        Assert.assertEquals(newCustomer, service.findById(NEW_CUSTOMER_ID));
     }
 
     @Test
     public void update() throws Exception {
-        Customer updated = new Customer(100000, "Anton");
+        Customer updated = UPDATED_CUSTOMER;
         service.save(updated);
-        Assert.assertEquals(updated, service.findById(100000));
+        Assert.assertEquals(updated, service.findById(CUSTOMER_ID1));
     }
 
     @Test
     public void getAll() throws Exception {
-        Assert.assertEquals(
-                service.findAll(),
-                List.of(
-                        new Customer(100000, "User"),
-                        new Customer(100001, "Admin"),
-                        new Customer(100002, "God")
-                )
-        );
+        Assert.assertEquals(service.findAll(), List.of(CUSTOMER_1, CUSTOMER_2, CUSTOMER_3));
     }
 
     @Test
     @Transactional
     public void delete() throws Exception {
-        service.delete(100000);
+        service.delete(CUSTOMER_ID1);
         Assert.assertEquals(service.findAll().size(), 2);
     }
 }
